d6410fde2ed2bf07d91921e426512caac9d08955
patte <p@tte.io>
2018-01-18 18:07:20 +0100

    log env

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { updateUserOnMailchimp } = require('@orbiting/backend-modules-mail')
 const { express: { assets } } = require('@orbiting/backend-modules-assets')
 
 module.exports.run = () => {
+  console.log(process.env)
   require('./lib/slackGreeter')
   const localModule = require('./graphql')
   const executableSchema = makeExecutableSchema(merge(localModule, [documents, redirections]))
